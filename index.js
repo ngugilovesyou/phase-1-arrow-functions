017f708301f6c3631e6cdb7d26af5998bdb5debe
Samuel <amsamgitau@gmail.com>
2024-06-26 12:37:39 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide=(num1, num2)=> {
+  return num1 / num2;
+}
+
+const num=2;
+const square = (num) =>{
+    return Math.pow(num, 2)
+} 
+const a=4;
+const b=6;
+const add=(a, b) => {
+    return a + b;
+}
